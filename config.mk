70f5c96597db703b243768f08ae2e6fb40c62b07
Stella Bloom <windowz414@gnuweeb.org>
2024-02-06 14:47:05 +0300

    kasumitones: Import initial tones
    * Ringtones for Your Gift To Me, CiRCLE THANKS MUSiC♪, Kyu~mai*flower
      and Shuwarin☆Dreaming
    
    * Notification tone for Your Gift To Me
    
    * Alarm tone for Your Gift To Me
    
    * Credits to imouto1308 for the edits.
    
    Signed-off-by: Stella Bloom <windowz414@gnuweeb.org>

---
+++ config.mk
@@ -0,0 +1,36 @@
+#
+# Kasumi Tones - Provides tone files specially made for Project Kasumi.
+# Copyright (C) 2024 Project Kasumi
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+# Tones to be added on our mod.
+
+# Poppin'Party - Your Gift To Me (Instrumental) - imouto1308 Edits
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ringtones/Your_Gift_To_Me.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Your_Gift_To_Me.ogg
+    $(LOCAL_PATH)/notifications/Your_Gift_To_Me.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Your_Gift_To_Me.ogg
+    $(LOCAL_PATH)/alarms/Your_Gift_To_Me.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Your_Gift_To_Me.ogg
+
+# Poppin'Party x Afterglow x Roselia x Pastel*Palettes x Hello, Happy World! x Morfonica x RAISE A SUILEN - CiRCLE THANKS MUSiC♪ (Instrumental) - imouto1308 Ringtone Edit
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ringtones/CiRCLE_THANKS_MUSiC.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/CiRCLE_THANKS_MUSiC.ogg
+
+# Pastel*Palettes - Kyu~mai*flower (Instrumental) - imouto1308 Ringtone Edit
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ringtones/Kyumai_flower.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Kyumai_flower.ogg
+
+# Pastel*Palettes - Shuwarin☆Dreaming (Instrumental) - imouto1308 Ringtone Edit
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ringtones/Shuwarin_Dreaming.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Shuwarin_Dreaming.ogg
